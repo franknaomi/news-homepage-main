6fdaa80acc206e7bd1640fedfc3731e472e0f085
franknaomi <franknaomi0@gmail.com>
2022-12-30 08:46:55 -0800

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+ function openNav() {
+    document.getElementById("sidenav").style.width = "50%";
+    x.classList.toggle("change");
+    
+  }
+
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0%";
+  }
